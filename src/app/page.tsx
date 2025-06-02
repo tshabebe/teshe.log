a84ce24248ac1138693d2d950f400f7029ec1b74
Teshome Abebe <161710654+tshabebe@users.noreply.github.com>
2025-06-02 22:59:41 +0000

    Add theme support with next-themes and update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="justify-items-center items-center gap-16 grid grid-rows-[20px_1fr_20px] p-8 sm:p-20 pb-20 min-h-screen font-[family-name:var(--font-geist-sans)]">
-      hello world
+      <h1 className="">hello world</h1>
     </div>
   )
 }
