e1979a6bbd4be431bf5ef620f30d2f3027875900
Teshome Abebe <161710654+tshabebe@users.noreply.github.com>
2025-06-03 09:45:45 +0000

    Update dependencies, refactor Home component, and enhance global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className="justify-items-center items-center gap-16 grid grid-rows-[20px_1fr_20px] p-8 sm:p-20 pb-20 min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <h1 className="">hello world</h1>
+    <div className="">
+      <header>hello world</header>
     </div>
   )
 }
